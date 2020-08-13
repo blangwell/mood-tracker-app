1b3c23602461da98a0844a47571ed3154492e3b0
blangwell <b.langwell@outlook.com>
2020-08-13 12:15:52 -0700

    [models] Add user validations and hooks

---
+++ models/user.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const bcrypt = require('bcrypt')
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  user.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: { //sequelize looks for this syntax
+          args: [1, 99],
+          msg: 'Name must be 1 to 99 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: { // sequelize validation
+          msg: 'Invalid email'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: 'Password must be between 8 and 99 characters'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+  user.addHook('beforeCreate', function(pendingUser) {
+    // we use function() because we are constructing
+    // remember fat arrows dont play nice with constructors
+    // hash the password 
+    let hash = bcrypt.hashSync(pendingUser.password, 12);
+    
+    // set the user password to the hash
+    pendingUser.password = hash;
+  })
+
+  user.prototype.validPassword = function(passwordTyped) {
+    let correctPassword = bcrypt.compareSync(passwordTyped, this.password);
+    // return true or false based on if pwd is correct
+    return correctPassword;
+  };
+
+  // remove the password before it gets serialized (create session and maneuver to app)
+  user.prototype.toJSON = function() {
+    let userData = this.get();
+    delete userData.password;
+    return userData;
+  }
+
+  return user;
+}
