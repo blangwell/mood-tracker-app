054b14cafdaacebf547a37be329811838353346c
blangwell <b.langwell@outlook.com>
2020-08-16 11:44:12 -0700

    [plotly] Import and test plotly API

--- apiTest.js
+++ apiTest.js
@@ -1,1 +1,15 @@
-const Chart = require('chart.js')
+const API_KEY = process.env.API_KEY;
+const plotly = require('plotly')({'username': 'blangwell', 'apiKey': API_KEY, 'host': 'http://localhost', 'port': 3000})
+
+let data = [
+    {
+        x: ['giraffes', 'orangutans', 'monkeys'],
+        y: [20, 14, 23],
+        type: 'bar'
+    }
+]
+
+let graphOptions = {filename: 'basic-bar', fileopt: 'overwrite'};
+plotly.plot(data, graphOptions, function (err, msg) {
+    console.log(msg);
+})
--- routes/track.js
+++ routes/track.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const db = require('../models');
 const router = express.Router();
+const API_KEY = process.env.API_KEY;
+const plotly = require('plotly')({'username': 'blangwell', 'apiKey': API_KEY})
 
 router.get('/', (req, res) => {
-    res.render('track/index')
+    let data = [
+        {
+            x: ['test1', 'test2', 'test3'],
+            y: [20, 14, 23],
+            type: 'bar'
+        }
+    ]
+    
+    let graphOptions = {filename: 'basic-bar', fileopt: 'overwrite'};
+    let newPlot = plotly.plot(data, graphOptions, function (err, msg) {
+        console.log(msg.url);
+    })
+    res.render('track/index', {plot: newPlot})
 })
 
 router.get('/new', (req, res) => {
     res.render('track/new')
 })
+
 
 
 
